a163e69e02c336d262b2fcc7ec41cbd78d74256f
madhuhema <madhunarmi@gmail.com>
2018-07-20 16:35:44 +0530

    testing for pattern done

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -6,6 +6,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 describe('FormComponent test', () => {
     let component: FormComponent;
     let fixture: ComponentFixture<FormComponent>;
+    let pattern = null;
     beforeEach(async() => {
       TestBed.configureTestingModule({
         declarations: [ FormComponent ],
@@ -15,16 +16,78 @@ describe('FormComponent test', () => {
       component = fixture.componentInstance;
 
     })
+  // checking whether the component is defined or not 
     it('is form component defined', () => {
     expect(component).toBeDefined();
   });
-  it('is form valid when empty',() => {
+  // checking whether the form fields does not hold null values
+  it('is form invalid when empty',() => {
+    //arrange
     let itemName = component.rform.controls["name"];
+    //act
     itemName.setValue("Mobile");
     let itemPrice = component.rform.controls["price"];
     itemName.setValue("10000");
     let itemShipping = component.rform.controls["shipping"];
     itemName.setValue("100");
-    expect(component.rform.valid).not.toBeNull();
+    //assert
+    expect(component.rform.valid).not.toBeTruthy();
   });
+  // checks whether the form is invalid if the price is less than 5000
+  it('is form invalid when price less than 5000',() => {
+    let itemName = component.rform.controls["name"];
+    itemName.setValue("Mobile");
+    //we have set the price below 5000 
+    let itemPrice = component.rform.controls["price"];
+    itemPrice.setValue("1000");
+    let itemShipping = component.rform.controls["shipping"];
+    itemShipping.setValue("100");
+    expect(component.rform.valid).toBeFalsy();
+    //test fails
+    expect(component.rform.controls["price"].valid).toBeFalsy();
+    //check which validation has failed
+    expect(itemPrice.errors["min"]).toBeDefined();
+  })
+  it('is form invalid when minimum length of name is not greater than or equal to 3',() => {
+    let itemName = component.rform.controls["name"];
+    //we have set the name with length less than 3
+    itemName.setValue("Mo");
+    let itemPrice = component.rform.controls["price"];
+    itemPrice.setValue("10000");
+    let itemShipping = component.rform.controls["shipping"];
+    itemShipping.setValue("100");
+    expect(component.rform.valid).toBeFalsy();
+    //test fails
+    expect(component.rform.controls["name"].valid).toBeFalsy();
+    //check which validation has failed
+    //expect(itemName.errors["minLength"]).toBeDefined();
+  })
+  //unit test for custom validator
+  it('is form invalid when product price is not divisible by 10',() => {
+    let itemName = component.rform.controls["name"];
+    //we have set the name with length less than 3
+    itemName.setValue("Mobile");
+    let itemPrice = component.rform.controls["price"];
+    itemPrice.setValue("5555");
+    let itemShipping = component.rform.controls["shipping"];
+    itemShipping.setValue("100");
+    expect(component.rform.valid).toBeFalsy();
+    //test fails
+    expect(component.rform.controls["price"].valid).toBeFalsy();
+    //check which validation has failed
+    expect(component.rform.controls["price"].errors["invalidPrice"]).toBeDefined();
+  })
+  it('is form invalid when  name field does not match pattern',() => {
+    let itemName = component.rform.controls["name"];
+    //we have set the name with length less than 3
+    itemName.setValue("Mobile123");
+    let itemPrice = component.rform.controls["price"];
+    itemPrice.setValue("10000");
+    let itemShipping = component.rform.controls["shipping"];
+    itemShipping.setValue("100");
+    expect(component.rform.valid).toBeFalsy();
+    //test fails
+    expect(component.rform.controls["name"].valid).toBeFalsy();
+    //expect(itemName.errors["pattern"]).toBeDefined();
+  })
 });
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule,ReactiveFormsModule,Validators,FormGroup,FormBuilder, FormGroupName} from '@angular/forms';
+import { PriceValidator } from './priceValidator';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -9,9 +10,9 @@ export class FormComponent implements OnInit {
    rform: FormGroup;
   constructor(private fb:FormBuilder) {
     this.rform = fb.group({
-      "name":['',Validators.required],
-      "price":['',Validators.required],
-      "shipping":['',Validators.required]
+      "name":['',[Validators.required,Validators.minLength(3),Validators.pattern('^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$')]],
+      "price":['',[Validators.required,Validators.min(4000),PriceValidator]],
+      "shipping":['',[Validators.required,Validators.min(100)]]
     })
    }
    
