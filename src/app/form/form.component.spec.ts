a4f4d39942f9e67cfb533b6e355edf2cd605e17f
madhuhema <madhunarmi@gmail.com>
2018-07-23 02:45:47 +0530

    final changes

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -1,27 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { FormComponent } from './form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from "./form.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-describe('FormComponent test', () => {
-    let component: FormComponent;
-    let fixture: ComponentFixture<FormComponent>;
-    let pattern = null;
-    beforeEach(async() => {
-      TestBed.configureTestingModule({
-        declarations: [ FormComponent ],
-        imports: [FormsModule,ReactiveFormsModule]
-      }).compileComponents();
-      fixture = TestBed.createComponent(FormComponent);
-      component = fixture.componentInstance;
-
-    })
-  // checking whether the component is defined or not 
-    it('is form component defined', () => {
+describe("FormComponent test", () => {
+  let component: FormComponent;
+  let fixture: ComponentFixture<FormComponent>;
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [FormComponent],
+      imports: [FormsModule, ReactiveFormsModule]
+    }).compileComponents();
+    fixture = TestBed.createComponent(FormComponent);
+    component = fixture.componentInstance;
+  });
+  // checking whether the component is defined or not
+  it("is form component defined", () => {
     expect(component).toBeDefined();
   });
   // checking whether the form fields does not hold null values
-  it('is form invalid when empty',() => {
+  it("is form invalid when empty", () => {
     //arrange
     let itemName = component.rform.controls["name"];
     //act
@@ -34,10 +32,10 @@ describe('FormComponent test', () => {
     expect(component.rform.valid).not.toBeTruthy();
   });
   // checks whether the form is invalid if the price is less than 5000
-  it('is form invalid when price less than 5000',() => {
+  it("is form invalid when price less than 5000", () => {
     let itemName = component.rform.controls["name"];
     itemName.setValue("Mobile");
-    //we have set the price below 5000 
+    //we have set the price below 5000
     let itemPrice = component.rform.controls["price"];
     itemPrice.setValue("1000");
     let itemShipping = component.rform.controls["shipping"];
@@ -47,8 +45,8 @@ describe('FormComponent test', () => {
     expect(component.rform.controls["price"].valid).toBeFalsy();
     //check which validation has failed
     expect(itemPrice.errors["min"]).toBeDefined();
-  })
-  it('is form invalid when minimum length of name is not greater than or equal to 3',() => {
+  });
+  it("is form invalid when minimum length of name is not greater than or equal to 3", () => {
     let itemName = component.rform.controls["name"];
     //we have set the name with length less than 3
     itemName.setValue("Mo");
@@ -61,9 +59,9 @@ describe('FormComponent test', () => {
     expect(component.rform.controls["name"].valid).toBeFalsy();
     //check which validation has failed
     //expect(itemName.errors["minLength"]).toBeDefined();
-  })
+  });
   //unit test for custom validator
-  it('is form invalid when product price is not divisible by 10',() => {
+  it("is form invalid when product price is not divisible by 10", () => {
     let itemName = component.rform.controls["name"];
     //we have set the name with length less than 3
     itemName.setValue("Mobile");
@@ -75,9 +73,11 @@ describe('FormComponent test', () => {
     //test fails
     expect(component.rform.controls["price"].valid).toBeFalsy();
     //check which validation has failed
-    expect(component.rform.controls["price"].errors["invalidPrice"]).toBeDefined();
-  })
-  it('is form invalid when  name field does not match pattern',() => {
+    expect(
+      component.rform.controls["price"].errors["invalidPrice"]
+    ).toBeDefined();
+  });
+  it("is form invalid when  name field does not match pattern", () => {
     let itemName = component.rform.controls["name"];
     //we have set the name with length less than 3
     itemName.setValue("Mobile123");
@@ -86,8 +86,70 @@ describe('FormComponent test', () => {
     let itemShipping = component.rform.controls["shipping"];
     itemShipping.setValue("100");
     expect(component.rform.valid).toBeFalsy();
-    //test fails
+    //test passes since the form validity and field validity is false
     expect(component.rform.controls["name"].valid).toBeFalsy();
     //expect(itemName.errors["pattern"]).toBeDefined();
-  })
+  });
+  //check whether the price value is set above 5000
+  it("is price value set correctly", () => {
+    let itemName = component.rform.controls["name"];
+    itemName.setValue("Mobile");
+    //we have set the price below 5000
+    let itemPrice = component.rform.controls["price"];
+    itemPrice.setValue("1000");
+    let itemShipping = component.rform.controls["shipping"];
+    itemShipping.setValue("100");
+    expect(itemPrice.value).toBeGreaterThan(4999);
+  });
+  //test for fizzbuzz function
+  it('is form invalid when the data field is empty ', () => {
+     //arrange
+     let number = component.rform.controls["data"];
+     //act-no value set for number
+     expect(component.rform.valid).toBeFalsy();
+     expect(component.rform.controls["data"].valid).toBeFalsy();
+     //required error becomes defined
+    expect(number.errors["required"]).toBeDefined();
+});
+  it('is form invalid when the data field is not a number ', () => {
+     //arrange
+     let number = component.rform.controls["data"];
+     //act
+     number.setValue("MOM")
+     expect(component.rform.valid).toBeFalsy();
+     expect(component.rform.controls["data"].valid).toBeFalsy();
+     //pattern error becomes defined
+    expect(number.errors["pattern"]).toBeDefined();
+});
+  //checks the output for a number divisible by both 3 and 5
+  it('is fizzbuzz function returns fizzbuzz ', () => {
+     //arrange
+     let number = component.rform.controls["data"];
+     //act
+     number.setValue("15");
+     expect(component.rform.valid).toBeFalsy();
+     expect(component.rform.controls["data"].valid).toBeTruthy();
+     expect(component.fizzBuzz(15)).toEqual(" Fizz,Buzz,Fizz,Fizz,Buzz,Fizz,FizzBuzz,");
+});
+//checks the output for number  3
+it('is fizzbuzz function returns fizz ', () => {
+  //arrange
+  let number = component.rform.controls["data"];
+  //act
+  number.setValue("3");
+  expect(component.rform.valid).toBeFalsy();
+  expect(component.rform.controls["data"].valid).toBeTruthy();
+  expect(component.fizzBuzz(3)).toEqual(" Fizz,");
+});
+//checks the output for a number 5
+it('is fizzbuzz function returns Buzz ', () => {
+  //arrange
+  let number = component.rform.controls["data"];
+  //act
+  number.setValue("5");
+  expect(component.rform.valid).toBeFalsy();
+  expect(component.rform.controls["data"].valid).toBeTruthy();
+  expect(component.fizzBuzz(5)).toEqual(" Fizz,Buzz,");
+});
+
 });
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -12,10 +12,26 @@ export class FormComponent implements OnInit {
     this.rform = fb.group({
       "name":['',[Validators.required,Validators.minLength(3),Validators.pattern('^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$')]],
       "price":['',[Validators.required,Validators.min(4000),PriceValidator]],
-      "shipping":['',[Validators.required,Validators.min(100)]]
+      "shipping":['',[Validators.required,Validators.min(100)]],
+       "data" :['',[Validators.required,Validators.pattern('^[0-9]*$')]]
     })
    }
-   
+    fizzBuzz(num) {
+    var n= num;
+    var ans=" ";
+    for(var i=1;i<=n;i++)
+    {
+        if(i % 3 == 0 && i % 5 !=0 )
+            ans+="Fizz,";
+        else if(i % 5 == 0 && i % 3 !=0)
+            ans+="Buzz,";
+        else if(i % 3 == 0 && i % 5 == 0)
+            ans+="FizzBuzz,";
+
+    }
+    return ans;
+}
+
   ngOnInit() {
   }
 
